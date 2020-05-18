35ac0b8da9af83ad8a059bc9d2e486eb51b90328
Dayana Rodriguez <dayana.rodriguez@epn.edu.ec>
2020-05-18 13:19:49 -0500

    Creacion de interface y logica de programacion para cargar una imagen al Storage

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_carpooling/src/pages/home_page.dart';
+import 'package:flutter_carpooling/src/pages/image_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes (){
   return <String, WidgetBuilder>{ 
         '/': (BuildContext context) => HomePage(),
+        'image': (BuildContext context) => ImageCapturePage()
       };
 }
