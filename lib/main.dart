ee20cc549909bff5e0dac5202df212ddf801ff9b
HectorAnaluisa <hector.analuisa@epn.edu.ec>
2020-05-18 00:01:11 -0500

    Creacion de interfaz busqueda de ruta

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: 'rutaUsual',
       routes: getAplicationRoutes(),
     );
   }
