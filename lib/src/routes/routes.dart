0371d33641a5609cfea36c9b9e34294a91671100
Dayana Rodriguez <dayana.rodriguez@epn.edu.ec>
2020-05-17 23:59:14 -0500

    Creacion de la interface de login y registro

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_carpooling/src/pages/home_page.dart';
+import 'package:flutter_carpooling/src/pages/login_pages.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes (){
   return <String, WidgetBuilder>{ 
         '/': (BuildContext context) => HomePage(),
+        'login': (BuildContext context) => LoginPage()
       };
 }
