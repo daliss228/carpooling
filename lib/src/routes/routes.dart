5da85dcae4b556eed4d4379cbb4a53cfea0644f9
HectorAnaluisa <hector.analuisa@epn.edu.ec>
2020-05-18 00:27:43 -0500

    Creacion interfaz de registro de automovil

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_carpooling/src/pages/home_page.dart';
+import 'package:flutter_carpooling/src/pages/registro_auto_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes (){
   return <String, WidgetBuilder>{ 
-        '/': (BuildContext context) => HomePage(),
+        '/'       : (BuildContext context) => HomePage(),
+        'regAuto' : (BuildContext context) => RegistroAutoPage(),
       };
 }
